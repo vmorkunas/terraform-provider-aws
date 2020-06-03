95ebc3bd96fdf80b4f5d89ccf41db0dff58bd9ae
Alex Pilon <apilon@hashicorp.com>
2020-06-02 20:42:54 -0400

    Reimplement with deep set support
    Reimplementation now takes single attr name and depth for finding potential matches.
    The implementation is more efficient and now supports deeply nested sets

--- aws/internal/test/testing.go
+++ aws/internal/test/testing.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -10,9 +9,9 @@ import (
 )
 
 // TestCheckTypeSetElemNestedAttrs is a resource.TestCheckFunc that accepts a resource
-// name and flatmap style key to a schema.TypeSet attribute. The function checks
-// if it appears to be a schema.TypeSet and then verifies that an element in
-// the set matches all nested attribute/value pairs.
+// name, an attribute name and depth which targets a TypeSet, as well as a value
+// map to verify. The function verifies that the TypeSet attribute exists, and that
+// an element matches all the values in the map.
 //
 // Use this function over SDK provided TestCheckFunctions when validating a
 // TypeSet where its elements are a nested object with their own attrs/values.
@@ -20,104 +19,93 @@ import (
 // Please note, if the provided value map is not granular enough, there exists
 // the possibility you match an element you were not intending to, in the TypeSet.
 // Provide a full mapping of attributes to be sure the unique element exists.
-func TestCheckTypeSetElemNestedAttrs(name, key string, values map[string]string) resource.TestCheckFunc {
+func TestCheckTypeSetElemNestedAttrs(resourceName, attrName string, depth int, values map[string]string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		ms := s.RootModule()
-		rs, ok := ms.Resources[name]
+		rs, ok := ms.Resources[resourceName]
 		if !ok {
-			return fmt.Errorf("Not found: %s in %s", name, ms.Path)
+			return fmt.Errorf("Not found: %s in %s", resourceName, ms.Path)
 		}
 
 		is := rs.Primary
 		if is == nil {
-			return fmt.Errorf("No primary instance: %s in %s", name, ms.Path)
+			return fmt.Errorf("No primary instance: %s in %s", resourceName, ms.Path)
 		}
 
-		// A TypeSet should have a special count attribute
-		countStr, ok := is.Attributes[key+".#"]
-		if !ok {
-			return fmt.Errorf("%q %q does not appear to be a TypeSet", name, key)
-		}
-		count, err := strconv.ParseInt(countStr, 10, 64)
-		if err != nil {
-			return err
-		}
+		matches := make(map[string]int)
 
-		// unflatten the TypeSet from State
-		passedKeyParts := strings.Split(key, ".")
-		elements := make(map[string]map[string]string, count)
 		for stateKey, stateValue := range is.Attributes {
-			stateKeyParts := strings.Split(stateKey, ".")
-			if strings.HasPrefix(stateKey, key) {
-				id := stateKeyParts[len(passedKeyParts)]
-				if id != "#" {
-					element, ok := elements[id]
-					if !ok {
-						elements[id] = make(map[string]string)
-						element = elements[id]
+			parts := strings.Split(stateKey, ".")
+			// a Set/List item with nested attrs would have a flatmap address of
+			// at least length 3
+			// foo.0.name = "bar"
+			d := len(parts) - 3
+			if d < 0 {
+				continue
+			}
+			attr := parts[d]
+			if attr == attrName && d == depth-1 {
+				// ensure this is a Set/List
+				if _, exists := is.Attributes[strings.Join(parts[:d+1], ".")+".#"]; !exists {
+					return fmt.Errorf("%q attr %q is not TypeSet", resourceName, attrName)
+				}
+				nestedAttr := parts[d+2]
+				elementId := parts[d+1]
+				// check if the nestedAttr exists in the passed values map
+				// if it does and matches, increment the matches count
+				if v, exists := values[nestedAttr]; exists && stateValue == v {
+					matches[elementId] = matches[elementId] + 1
+					// exit if there is an element that is a full match
+					if matches[elementId] == len(values) {
+						return nil
 					}
-					element[strings.Join(stateKeyParts[len(passedKeyParts)+1:], ".")] = stateValue
-					elements[id] = element
 				}
 			}
 		}
 
-		// sanity check
-		if len(elements) != int(count) {
-			return fmt.Errorf("Expected the number of set items to be %d, got %d.\nState: %#v", count, len(elements), is.Attributes)
-		}
-
-		// check if an element is a full match with the passed values map
-		for _, element := range elements {
-			var matches int
-			for k, v := range values {
-				if stateValue, keyExists := element[k]; keyExists && stateValue == v {
-					matches++
-				}
-			}
-			if matches == len(values) {
-				return nil
-			}
-		}
-
-		return fmt.Errorf("%q %q has no element with attr/value pairs: %#v in state: %#v", name, key, values, is.Attributes)
+		return fmt.Errorf("No TypeSet element in %q with attr name %q at depth %d, with nested attrs %#v in state: %#v", resourceName, attrName, depth, values, is.Attributes)
 	}
 }
 
 // TestCheckTypeSetElemAttr is a resource.TestCheckFunc that accepts a resource
-// name and flatmap style key to a schema.TypeSet attribute. The function checks
-// if it appears to be a schema.TypeSet and then verifies that an element in
-// the set matches the passed value.
+// name, an attribute name and depth which targets a TypeSet, as well as a value
+// to verify. The function verifies that the TypeSet attribute exists, and that
+// an element matches the passed value.
 //
 // Use this function over SDK provided TestCheckFunctions when validating a
 // TypeSet where its elements are a simple value
-func TestCheckTypeSetElemAttr(name, key, value string) resource.TestCheckFunc {
+func TestCheckTypeSetElemAttr(resourceName, attrName string, depth int, value string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		ms := s.RootModule()
-		rs, ok := ms.Resources[name]
+		rs, ok := ms.Resources[resourceName]
 		if !ok {
-			return fmt.Errorf("Not found: %s in %s", name, ms.Path)
+			return fmt.Errorf("Not found: %s in %s", resourceName, ms.Path)
 		}
 
 		is := rs.Primary
 		if is == nil {
-			return fmt.Errorf("No primary instance: %s in %s", name, ms.Path)
-		}
-
-		// A TypeSet should have a special count attribute
-		if _, ok := is.Attributes[key+".#"]; !ok {
-			return fmt.Errorf("%q %q does not appear to be a TypeSet", name, key)
+			return fmt.Errorf("No primary instance: %s in %s", resourceName, ms.Path)
 		}
 
 		for stateKey, stateValue := range is.Attributes {
 			parts := strings.Split(stateKey, ".")
-			// ensure the passed key is in fact the direct path to the supposed
-			// TypeSet and the values match
-			if stateValue == value && key == strings.Join(parts[:len(parts)-1], ".") {
-				return nil
+			// a Set/List item would have a flatmap address of at least length 2
+			// foo.0 = "bar"
+			d := len(parts) - 2
+			if d < 0 {
+				continue
+			}
+			attr := parts[d]
+			if attr == attrName && d == depth-1 && stateValue == value {
+				// ensure this is a Set/List
+				if _, exists := is.Attributes[strings.Join(parts[:d+1], ".")+".#"]; !exists {
+					return fmt.Errorf("%q attr %q is not TypeSet", resourceName, attrName)
+				} else {
+					return nil
+				}
 			}
 		}
 
-		return fmt.Errorf("%q %q has no element with value: %q in state: %#v", name, key, value, is.Attributes)
+		return fmt.Errorf("No TypeSet element in %q with attr name %q at depth %d, with value %q in state: %#v", resourceName, attrName, depth, value, is.Attributes)
 	}
 }
